0268b6ce7d363a96d942ffcbf60c8ec4bdc4aa4e
pradip <pradip9102@gmail.com>
2018-06-17 22:29:33 +0530

    Initial commit

---
+++ src/main/kotlin/com/vizurd/travel/mfindo/MFindoApplication.kt
@@ -0,0 +1,17 @@
+package com.vizurd.travel.mfindo
+
+import mu.KotlinLogging
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MFindoApplication {
+    companion object {
+        val logger = KotlinLogging.logger {  }
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<MFindoApplication>(*args)
+    MFindoApplication.logger.debug { "m-Findo is up and running." }
+}
