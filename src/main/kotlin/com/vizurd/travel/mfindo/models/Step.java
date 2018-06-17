5d8cb692b082335316ba265a0f7ea17fd3ef7049
pradip <pradip9102@gmail.com>
2018-06-18 01:00:34 +0530

    Initial commit

--- src/main/kotlin/com/vizurd/travel/mfindo/models/Step.java
+++ src/main/kotlin/com/vizurd/travel/mfindo/models/Step.java
@@ -7,46 +7,46 @@ public class Step {
 
     @SerializedName("distance")
     @Expose
-    private Distance_ distance;
+    private Object distance;
     @SerializedName("duration")
     @Expose
-    private Duration_ duration;
+    private Object duration;
     @SerializedName("end_location")
     @Expose
-    private EndLocation_ endLocation;
+    private Object endLocation;
     @SerializedName("start_location")
     @Expose
-    private StartLocation_ startLocation;
+    private Object startLocation;
 
-    public Distance_ getDistance() {
+    public Object getDistance() {
         return distance;
     }
 
-    public void setDistance(Distance_ distance) {
+    public void setDistance(Object distance) {
         this.distance = distance;
     }
 
-    public Duration_ getDuration() {
+    public Object getDuration() {
         return duration;
     }
 
-    public void setDuration(Duration_ duration) {
+    public void setDuration(Object duration) {
         this.duration = duration;
     }
 
-    public EndLocation_ getEndLocation() {
+    public Object getEndLocation() {
         return endLocation;
     }
 
-    public void setEndLocation(EndLocation_ endLocation) {
+    public void setEndLocation(Object endLocation) {
         this.endLocation = endLocation;
     }
 
-    public StartLocation_ getStartLocation() {
+    public Object getStartLocation() {
         return startLocation;
     }
 
-    public void setStartLocation(StartLocation_ startLocation) {
+    public void setStartLocation(Object startLocation) {
         this.startLocation = startLocation;
     }
 
