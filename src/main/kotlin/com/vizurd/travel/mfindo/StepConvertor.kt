2569c550b6375879a60b674b86923b9fd4f4b576
Sanoop Sashindran <sanoop.sashindran@iifl.com>
2018-06-19 00:12:39 +0530

    common path logic written

--- src/main/kotlin/com/vizurd/travel/mfindo/StepConvertor.kt
+++ src/main/kotlin/com/vizurd/travel/mfindo/StepConvertor.kt
@@ -6,21 +6,26 @@ import com.google.maps.model.DirectionsStep
 import com.google.maps.model.TravelMode
 import com.vizurd.travel.mfindo.models.CommonStepUser
 import com.vizurd.travel.mfindo.models.UserStep
+import mu.KotlinLogging
+import java.io.File
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 class StepConvertor {
+
+    val logger = KotlinLogging.logger { }
 
     fun fetchFromGeoApi() {
 //        val gson = GsonBuilder().setPrettyPrinting().create()
         val context = RetrofitProvider().getGeoApiContext()
         val directionRequest1 = DirectionsApi.newRequest(context)
-                .origin("Kurla")
-                .destination("ville parle")
+                .origin("bandra")
+                .destination("borivali")
                 .mode(TravelMode.DRIVING)
         val directionRequest2 = DirectionsApi.newRequest(context)
-                .origin("Kurla")
-                .destination("ville parle")
+                .origin("ghatkopar")
+                .destination("borivali")
                 .mode(TravelMode.DRIVING)
         val result1 = directionRequest1.await()
         val result2 = directionRequest2.await()
@@ -48,11 +53,24 @@ class StepConvertor {
         val usersSteps = ArrayList<UserStep>()
         steps1.forEach { usersSteps.add(UserStep(it, user1)) }
         steps2.forEach { usersSteps.add(UserStep(it, user2)) }
+        val step3 = Arrays.copyOf(steps1, steps1.size + steps2.size)
+        System.arraycopy(steps2, 0, step3, steps1.size, steps2.size)
+        val stepList = step3.asList().distinctBy { Pair(it.startLocation, it.endLocation) }
+        var commonStepUser = ArrayList<CommonStepUser>()
+        logger.debug { "Step1 size -> ${steps1.size}" }
+        logger.debug { "Step2 size -> ${steps2.size}" }
+        logger.debug { "Step3 size -> ${step3.size}" }
+        logger.debug { "stepList size -> ${stepList.size}" }
 
-//        val step3 =
-        val commonStepUser = ArrayList<CommonStepUser>()
-        steps1.forEach { step ->
-            val commonnSteps = usersSteps.filter { uStep -> uStep.step!!.equals(step) }
+        printLatLong(steps1, fileName = "step1.txt")
+        printLatLong(steps2, fileName = "step2.txt")
+        stepList.forEach { step ->
+            val commonnSteps = usersSteps.filter { uStep ->
+                (uStep.step!!.startLocation!!.equals(step.startLocation)
+                        && (uStep.step!!.endLocation!!.equals(step.endLocation)))
+            }
+            logger.debug { "common steps size -> ${commonnSteps.size}" }
+//            println(gson.toJson(commonnSteps))
             val users = ArrayList<String>()
             val commonUserStep = CommonStepUser(step, users)
             commonnSteps.forEach {
@@ -60,6 +78,17 @@ class StepConvertor {
             }
             commonStepUser.add(commonUserStep)
         }
+        commonStepUser = commonStepUser.filter { stUser -> stUser.users.size > 1 } as ArrayList
+        logger.debug { "commonStepUser size -> ${commonStepUser.size}" }
         println(gson.toJson(commonStepUser))
+    }
+
+    fun printLatLong(steps: Array<DirectionsStep>, fileName: String) {
+        File(fileName).printWriter().use { out ->
+            steps.forEach {
+                out.println("start location: [${it.startLocation.lat}, ${it.startLocation.lng}]," +
+                        "end location: [${it.endLocation.lat}, ${it.endLocation.lng}]")
+            }
+        }
     }
 }
