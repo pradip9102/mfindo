5879b137653212b4be6af0671c285e9bdcce37fe
Sanoop Sashindran <sanoop.sashindran@iifl.com>
2018-06-20 01:14:02 +0530

    Adding class objects to make code readable

--- src/main/kotlin/com/vizurd/travel/mfindo/controllers/MapRestController.kt
+++ src/main/kotlin/com/vizurd/travel/mfindo/controllers/MapRestController.kt
@@ -2,7 +2,9 @@ package com.vizurd.travel.mfindo.controllers
 
 import com.google.maps.DirectionsApi
 import com.google.maps.model.DirectionsStep
-import com.vizurd.travel.mfindo.models.CommonStepUser
+import com.vizurd.travel.mfindo.models.CommonStepsForUsers
+import com.vizurd.travel.mfindo.models.UserDirectionResult
+import com.vizurd.travel.mfindo.models.UserRawStepModel
 import com.vizurd.travel.mfindo.models.UserStep
 import com.vizurd.travel.mfindo.services.GoogleMapsService
 import com.vizurd.travel.mfindo.utils.GsonUtil
@@ -21,26 +23,42 @@ class MapRestController(
     val logger = KotlinLogging.logger { }
 
     @GetMapping("/maps/api/result")
-    fun getResult(@RequestParam("origin1") origin1: String, @RequestParam("target1") target1: String, @RequestParam("origin2") origin2: String, @RequestParam("target2") target2: String): ResponseEntity<ArrayList<CommonStepUser>> {
-        val result1 = DirectionsApi.getDirections(googleMapsService.getContext(), origin1, target1).await()
-        val result2 = DirectionsApi.getDirections(googleMapsService.getContext(), origin2, target2).await()
-        val commonPath = findCommonPath("A", result1.routes[0].legs[0].steps.toList(), "B", result2.routes[0].legs[0].steps.toList())
+    fun getResult(@RequestParam("origin1") origin1: String, @RequestParam("target1") target1: String, @RequestParam("origin2") origin2: String, @RequestParam("target2") target2: String): ResponseEntity<ArrayList<CommonStepsForUsers>> {
+        val resultList = ArrayList<UserDirectionResult>()
+        resultList.add(UserDirectionResult("A", DirectionsApi.getDirections(googleMapsService.getContext(), origin1, target1).await()))
+        resultList.add(UserDirectionResult("B", DirectionsApi.getDirections(googleMapsService.getContext(), origin2, target2).await()))
+        val commonPath = findCommonPath(getUserStepRawFromDirectionResult(resultList))
         return ResponseEntity(commonPath, HttpStatus.OK)
     }
 
-    private fun findCommonPath(user1: String, steps1: List<DirectionsStep>, user2: String, steps2: List<DirectionsStep>): ArrayList<CommonStepUser> {
+    private fun getUserStepRawFromDirectionResult(userResults: ArrayList<UserDirectionResult>): UserRawStepModel {
+        val users = ArrayList<String>()
+        val directionsStep = ArrayList<Array<DirectionsStep>>()
+        userResults.forEach { result ->
+            users.add(result.user)
+            directionsStep.add(result.result.routes[0].legs[0].steps)
+        }
+        val rawSteps = UserRawStepModel(users, directionsStep)
+        return rawSteps
+    }
+
+    private fun findCommonPath(userRaw: UserRawStepModel): ArrayList<CommonStepsForUsers> {
+        val user1 = userRaw.users[0]
+        val steps1 = userRaw.steps[0]
+        val user2 = userRaw.users[1]
+        val steps2 = userRaw.steps[1]
         val allUserSteps = steps1.map { UserStep(it, user1) } + steps2.map { UserStep(it, user2) }
         val allSteps = (steps1 + steps2).distinctBy { Pair(it.startLocation, it.endLocation) }
 
-        var commonStepUser = ArrayList<CommonStepUser>()
+        var commonStepUser = ArrayList<CommonStepsForUsers>()
 
         allSteps.forEach { step ->
-            val commonUserStep = CommonStepUser(step, arrayListOf())
+            val commonStepsForUsers = CommonStepsForUsers(step, arrayListOf())
             allUserSteps
                     .filter { it.step!!.startLocation!! == step.startLocation }
                     .filter { it.step!!.endLocation!! == step.endLocation }
-                    .forEach { commonUserStep.users.add(it.users) }
-            commonStepUser.add(commonUserStep)
+                    .forEach { commonStepsForUsers.users.add(it.users) }
+            commonStepUser.add(commonStepsForUsers)
         }
         commonStepUser = commonStepUser.filter { stUser -> stUser.users.size > 1 } as ArrayList
         logger.debug { "commonStepUser size -> ${commonStepUser.size}" }
@@ -50,7 +68,7 @@ class MapRestController(
         return commonStepUser
     }
 
-    private fun printLatLong(steps: List<DirectionsStep>, fileName: String) {
+    private fun printLatLong(steps: Array<DirectionsStep>, fileName: String) {
         File(fileName).printWriter().use { out ->
             steps.forEach {
                 out.println("start location: [${it.startLocation.lat}, ${it.startLocation.lng}]," +
--- src/main/kotlin/com/vizurd/travel/mfindo/models/UserSteps.kt
+++ src/main/kotlin/com/vizurd/travel/mfindo/models/UserSteps.kt
@@ -1,6 +1,9 @@
 package com.vizurd.travel.mfindo.models
 
+import com.google.maps.model.DirectionsResult
 import com.google.maps.model.DirectionsStep
 
 data class UserStep(var step: DirectionsStep?, var users: String)
-data class CommonStepUser(var step: DirectionsStep?, var users: ArrayList<String>)
+data class CommonStepsForUsers(var step: DirectionsStep?, var users: ArrayList<String>)
+data class UserDirectionResult(var user: String,var result: DirectionsResult)
+data class UserRawStepModel(var users: ArrayList<String>,var steps: ArrayList<Array<DirectionsStep>>)
