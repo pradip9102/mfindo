e9ffe3af116c175815134a5e89dad9e355bdc626
imsourya <nitj.saurabh@gmail.com>
2018-06-25 18:11:13 +0530

    ->Adding Longest Common path Algo ->Adding Exception Module to list all Application level Custom Exceptions

--- src/main/kotlin/com/vizurd/travel/mfindo/controllers/MapRestController.kt
+++ src/main/kotlin/com/vizurd/travel/mfindo/controllers/MapRestController.kt
@@ -5,9 +5,7 @@ import com.google.maps.model.DirectionsStep
 import com.vizurd.travel.mfindo.models.CommonStepsForUsers
 import com.vizurd.travel.mfindo.models.UserDirectionResult
 import com.vizurd.travel.mfindo.models.UserRawStepModel
-import com.vizurd.travel.mfindo.models.UserStep
 import com.vizurd.travel.mfindo.services.GoogleMapsService
-import com.vizurd.travel.mfindo.utils.GsonUtil
 import mu.KotlinLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -42,29 +40,88 @@ class MapRestController(
         return rawSteps
     }
 
-    private fun findCommonPath(userRaw: UserRawStepModel): ArrayList<CommonStepsForUsers> {
-        val user1 = userRaw.users[0]
+    private fun findCommonPath(userRaw: UserRawStepModel): ArrayList<CommonStepsForUsers>
+    {
+        var temp = ArrayList<CommonStepsForUsers>()
+
+        val user1  = userRaw.users[0]
+        val user2  = userRaw.users[1]
         val steps1 = userRaw.steps[0]
-        val user2 = userRaw.users[1]
-        val steps2 = userRaw.steps[1]
-        val allUserSteps = steps1.map { UserStep(it, user1) } + steps2.map { UserStep(it, user2) }
-        val allSteps = (steps1 + steps2).distinctBy { Pair(it.startLocation, it.endLocation) }
+        val steps2  = userRaw.steps[1]
 
-        var commonStepUser = ArrayList<CommonStepsForUsers>()
-
-        allSteps.forEach { step ->
-            val commonStepsForUsers = CommonStepsForUsers(step, arrayListOf())
-            allUserSteps
-                    .filter { it.step!!.startLocation!! == step.startLocation }
-                    .filter { it.step!!.endLocation!! == step.endLocation }
-                    .forEach { commonStepsForUsers.users.add(it.users) }
-            commonStepUser.add(commonStepsForUsers)
+        if(user1 != user2)  /* For Same USer it wont calculate*/
+        {
+            logger.debug { "Users going to looking at common path" }
+            logger.debug { "Length User 1 "+steps1.size+" length User 2 "+steps2.size }
+            //printLatLong(steps1,"abc1.txt")
+            //printLatLong(steps2,"abc2.txt")
+            temp = LCPath(steps1,steps2,steps1.size,steps2.size)
         }
-        commonStepUser = commonStepUser.filter { stUser -> stUser.users.size > 1 } as ArrayList
-        logger.debug { "commonStepUser size -> ${commonStepUser.size}" }
-        println(GsonUtil.prettyJson(commonStepUser))
-        printLatLong(steps1, fileName = "step1.txt")
-        printLatLong(steps2, fileName = "step2.txt")
+        else
+        {
+            logger.warn { "Same User Finding Common Path : "+user1+" "+user2 }
+        }
+        return temp
+    }
+
+    private fun matchPath(userSteps1 : Array<DirectionsStep>,userSteps2 :Array<DirectionsStep>, m:Int , n:Int): Boolean
+    {
+        if(userSteps1[m].startLocation.lat != userSteps2[n].startLocation.lat) return false
+        if(userSteps1[m].startLocation.lng != userSteps2[n].startLocation.lng) return false
+        if(userSteps1[m].endLocation.lat != userSteps2[n].endLocation.lat) return false
+        if(userSteps1[m].endLocation.lng != userSteps2[n].endLocation.lng) return false
+        if(userSteps1[m].distance.inMeters != userSteps2[n].distance.inMeters) return false
+
+        return true
+    }
+
+    private fun LCPath(userSteps1 : Array<DirectionsStep>,userSteps2 :Array<DirectionsStep>, m:Int , n:Int): ArrayList<CommonStepsForUsers> {
+        logger.debug { "Entering Longest common Path" }
+        val LCLen = Array(m+1, {IntArray(n+1)})
+        var result =0
+        var row = 0
+        var col = 0
+        for(i in 0..m)
+        {
+            for(j in 0..n)
+            {
+                if (i == 0 || j == 0)
+                    LCLen[i][j] = 0
+                else if (matchPath(userSteps1,userSteps2,i-1,j-1))
+                {
+                    LCLen[i][j] = LCLen[i - 1][j - 1] + 1
+                    if(result < LCLen[i][j]) {
+                        result = LCLen[i][j]
+                        row = i
+                        col = j
+                    }
+                }
+                else
+                {
+                    LCLen[i][j] = 0
+                }
+            }
+        }
+        if(result == 0)
+        {
+            logger.debug { "no Common Path" }
+        }
+        else
+        {
+            logger.debug { "Found Common Path in numbers of steps: " +result}
+        }
+
+        val commonStepUser = ArrayList<CommonStepsForUsers>()
+
+        while (LCLen[row][col] != 0) {
+            commonStepUser.add(CommonStepsForUsers(userSteps1[row-1], arrayListOf()))
+            row--
+            col-- /* Could be removed*/
+        }
+        commonStepUser.forEach{ it->
+            logger.debug {it.step!!.startLocation.lat.toString() + "  "+ it.step!!.startLocation.lng +" "+ it.step!!.endLocation.lat +"  "+ it.step!!.endLocation.lng}
+        }
+        logger.debug { "leaving Longest common Path" }
         return commonStepUser
     }
 
@@ -76,5 +133,4 @@ class MapRestController(
             }
         }
     }
-
 }
---
+++ src/main/kotlin/com/vizurd/travel/mfindo/exceptions/MFindoDirectionApiPathNotFound.kt
@@ -0,0 +1,3 @@
+package com.vizurd.travel.mfindo.exceptions
+
+class DirectionApiPathNotFoundException(override var message:String): Exception(message)
